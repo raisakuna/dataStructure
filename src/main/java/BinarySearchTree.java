acc33d7d5783332bb70c9df744a579665a37139b
raisakuna <sakunaxl@gmail.com>
2024-09-03 17:30:29 -0400

    small changes

--- src/main/java/BinarySearchTree.java
+++ src/main/java/BinarySearchTree.java
@@ -2,9 +2,57 @@ class  Node{
     int data;
     Node left;
     Node right;
+
+    public Node(int data){
+        this.data =data;
+    }
         }
  class BinarySearchTree {
-    public static void main(String[] args) {
+    Node root;
+
+    public void insert(int data){
+        root = insertRec(root, data);
+    }
+    public Node insertRec(Node root, int data){
+        if (root==null)
+            root=new Node(data);
+        else if (data<root.data)
+            root.left =insertRec(root.left, data);
+        else if(data > root.data)
+            root.right =insertRec(root.right,data);
+        return root;
+    }
+
+    // Inorder traverse left-root-right
+     public void inorder(){
+        inorderRec(root);
+     }
+
+     private void inorderRec(Node root) {
+         if (root != null) {
+             inorderRec(root.left);
+             System.out.println(root.data + " ");
+             inorderRec(root.right);
+         }
+     }
+        // PreOrder root-left-right
+         public void preOrder(){
+            preOrderRec(root);
+
+     }
+
+     private void preOrderRec(Node root) {
+        if (root != null){
+            System.out.println(root.data + " ");
+            preOrderRec(root.left);
+            preOrderRec(root.right);
+
+        }
+
+        // PostOrder Left-Right-Root
+     }
+
+     public static void main(String[] args) {
 
     }
 }
